8260e9c50b915d61c9d06811ddf736bd6c8861d8
git-riyaz <riyaz.skk1@gmail.com>
2024-01-21 01:21:42 +0530

    Background Color Modify

--- js/script.js
+++ js/script.js
@@ -1,29 +1,32 @@
 var swiper = new Swiper(".slide-content", {
-    slidesPerView: 3,
-    spaceBetween: 25,
-    loop: true,
-    centerSlide: 'true',
-    fade: 'true',
-    grabCursor: 'true',
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets: true,
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  autoplay: {
+    delay: 1500, // autoplay delay in milliseconds
+    direction: 'reverse' // move slides from right to left
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints:{
+    0: {
+        slidesPerView: 1,
     },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+    520: {
+        slidesPerView: 2,
     },
-
-    breakpoints:{
-        0: {
-            slidesPerView: 1,
-        },
-        520: {
-            slidesPerView: 2,
-        },
-        950: {
-            slidesPerView: 3,
-        },
+    950: {
+        slidesPerView: 3,
     },
-  });
+  },
+});
