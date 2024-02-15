a26c95a15df5471b4ed874c75cb29be2dbfce2a3
git-riyaz <riyaz.skk1@gmail.com>
2024-02-16 01:26:54 +0530

    Update message.js

--- js/message.js
+++ js/message.js
@@ -3,8 +3,8 @@ function sendEmail(){
         Host : "smtp.elasticemail.com",
         Username : "riyaz.pandas@gmail.com",
         Password : "EA4AC11A84D218B239367D88A4B7B19FF6CA",
-        To : 'official@skriyaz.co',
-        From : 'riyaz.pandas@gmail.com',
+        To : 'skriyaz.dev@gmail.com',
+        From : 'official@skriyaz.co',
         Subject : "New Entry to Your Portfolio",
         Body : "Name: " + document.getElementById("name").value
         + "<br>Email: " + document.getElementById("email").value
